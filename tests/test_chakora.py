222e508083ea50756d21327351dbb974c8ca4b5d
Sai Teja <saitejatatineni5679@gmail.com>
2026-09-10 13:27:01 +0530

    Add home page Selenium test

--- tests/test_chakora.py
+++ tests/test_chakora.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
 
 
-def test_chakora_website():
+def test_chakora_home_page():
     driver = webdriver.Chrome()
 
-    driver.get("https://chakorahub.com")
+    try:
+        driver.get("https://chakorahub.com")
 
-    assert "Chakora" in driver.title
+        # Verify page title
+        assert "ChakoraHub" in driver.title
 
-    driver.quit()
+        # Verify Home Page content
+        assert "Login to ChakoraHub" in driver.page_source
+        assert "Our Services" in driver.page_source
+        assert "Quick Enquiry" in driver.page_source
+
+    finally:
+        driver.quit()
